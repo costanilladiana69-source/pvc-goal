3d908d1c98dd53c3fc0261c18c32aaca90dec870
Lovenel27 <lovenelcanillo@gmail.com>
2025-08-27 15:34:54 +0800

    Fire config

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAk-3TJk6Il_IHjwXPJCxJQK8G0vnJclZw",
+  authDomain: "pvc-goal.firebaseapp.com",
+  projectId: "pvc-goal",
+  storageBucket: "pvc-goal.firebasestorage.app",
+  messagingSenderId: "1062708504354",
+  appId: "1:1062708504354:web:0be6da761f113bd66ee77b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
