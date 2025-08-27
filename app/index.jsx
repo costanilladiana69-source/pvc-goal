c3ed866b7a1f0dba1fdf96d5ef338d1050113d99
costanilladiana69-source <costanilladiana69@gmail.com>
2025-08-27 16:05:39 +0800

    firebase configuration

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        P V C G O A L S
+        P V C GOALS
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
